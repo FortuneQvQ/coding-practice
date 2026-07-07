9bf9c878f46d69624a32853018b7982670647afc
BoBoYeYe193 <2912005208@qq.com>
2026-07-07 14:02:15 +0800

    添加接口文件
    这是大家共享的接口文件

---
+++ News.h
@@ -0,0 +1,19 @@
+#ifndef NEWS_H
+#define NEWS_H
+
+#include <string>
+
+struct News
+{
+    std::string title;      // 标题
+
+    std::string time;       // 发布时间
+
+    std::string content;    // 正文
+
+    std::string url;        // 原链接（唯一ID）
+
+    std::string source;     // 来源
+};
+
+#endif
