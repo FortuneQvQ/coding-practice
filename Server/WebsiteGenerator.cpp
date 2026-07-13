0a26e1a91db76b0bf233a97f03a017ff9f27ea6e
BoBoYeYe193 <2912005208@qq.com>
2026-07-13 16:55:20 +0800

    修复2

---
+++ Server/WebsiteGenerator.cpp
@@ -0,0 +1,454 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<vector>
+#include<filesystem>
+
+#include "database.h"
+#include "other/Category.h"
+
+using namespace std;
+
+
+//获取HTML模板
+string readTemplate(const string& templateFile) {
+
+    ifstream file(templateFile, ios::binary);
+    if (!file.is_open()) {
+        cerr << "Failed to open template file: " << templateFile << endl;
+        return "";
+    }
+    stringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+    return buffer.str();
+
+}
+
+
+//字符串替换函数
+void replace(string &html, string key, string value){
+
+    size_t pos;
+    while((pos = html.find(key)) != string::npos){
+        html.replace(pos, key.length(), value);
+    }
+
+}
+
+
+//生成单个新闻详情页
+void generateNewsDetailPage(const News& news) {
+
+    //读取HTML模板
+    string html = readTemplate("templates/news_detail.html");
+    if (html.empty()) {
+        cerr << "Template is empty. Cannot generate news detail page." << endl;
+        return;
+    }
+
+    //进行字符串替换
+    replace(html, "{{title}}", news.title);
+    replace(html, "{{time}}", news.time);
+    replace(html, "{{content}}", news.content);
+    replace(html, "{{url}}", news.url);
+    replace(html, "{{source}}", news.source);
+    string imageBlock;
+    if(!news.image.empty() && filesystem::exists("output/" + news.image)){
+        imageBlock = "<img class=\"news-image\" src=\"../" + news.image + "\">";
+    }
+    else{
+        imageBlock = "";
+    }   
+    replace(html, "{{imageBlock}}",imageBlock);
+    replace(html, "{{abstract}}", news.abstract);
+
+    //生成HTML文件
+    string filename = "output/detail/" + to_string(news.id) + ".html";
+    ofstream outFile(filename, ios::binary);
+    if (outFile.is_open()) {
+        outFile << html;
+        outFile.close();
+    } else {
+        cerr << "Failed to open output file: " << filename << endl;
+    }
+
+}
+
+
+//生成最新新闻卡片HTML(数量由count决定）（用于生成新闻主页）
+string generateLatestNewsCards(const vector<News>& newsList)
+{
+
+    string newsBlock;
+
+    int count = 0;
+
+    for(auto& news : newsList)
+    {
+        if(count >= 3)
+            break;
+
+        newsBlock += "<div class=\"news-card\">";
+         //来源
+        newsBlock += "<div class=\"news-source\">";
+        newsBlock += news.source;
+        newsBlock += "</div>";
+        //标题
+        newsBlock += "<div class=\"news-title\">";
+        newsBlock += news.title;
+        newsBlock += "</div>";
+        //时间
+        newsBlock += "<div class=\"news-time\">";
+        newsBlock += u8"发布时间：";
+        newsBlock += news.time;
+        newsBlock += "</div>";
+        //摘要
+        newsBlock += "<div class=\"news-abstract\">";
+        newsBlock += news.abstract;
+        newsBlock += "</div>";
+        //详情按钮
+        newsBlock += "<a class=\"detail-btn\" href=\"detail/";
+        newsBlock += to_string(news.id);
+        newsBlock += ".html?from=index\">";
+        newsBlock += u8"查看详情";
+        newsBlock += "</a>";
+        newsBlock += "</div>";
+
+        count++;
+    }
+
+    return newsBlock;
+
+}
+
+
+//生成所有新闻卡片HTML（用于生成分类详情页）
+string generateAllNewsCards(const vector<News>& newsList, const Category& category)
+{
+
+    string newsBlock;
+
+    for(auto& news : newsList)
+    {
+        newsBlock += "<div class=\"news-card\">";
+         //来源
+        newsBlock += "<div class=\"news-source\">";
+        newsBlock += news.source;
+        newsBlock += "</div>";
+        //标题
+        newsBlock += "<div class=\"news-title\">";
+        newsBlock += news.title;
+        newsBlock += "</div>";
+        //时间
+        newsBlock += "<div class=\"news-time\">";
+        newsBlock += u8"发布时间：";
+        newsBlock += news.time;
+        newsBlock += "</div>";
+        //摘要
+        newsBlock += "<div class=\"news-abstract\">";
+        newsBlock += news.abstract;
+        newsBlock += "</div>";
+        //详情按钮
+        newsBlock += "<a class=\"detail-btn\" href=\"../detail/";
+        newsBlock += to_string(news.id);
+        newsBlock += ".html?from=";
+        newsBlock += category.filename;
+        newsBlock += "\">";
+        newsBlock += u8"查看详情";
+        newsBlock += "</a>";
+        newsBlock += "</div>";
+    }
+
+    return newsBlock;
+
+}
+
+
+//生成新闻首页
+void generateNewsIndexPage(const vector<News>& newsList) {
+
+    //读取HTML模板
+    string html = readTemplate("templates/index.html");
+    if (html.empty()) {
+        cerr << "Template is empty. Cannot generate news index page." << endl;
+        return;
+    }
+
+    //生成新闻列表HTML
+    string newsListHtml = generateLatestNewsCards(newsList);
+
+    //将新闻列表HTML插入到首页模板中
+    replace(html, "{{news_list}}", newsListHtml);
+
+    //生成HTML文件
+    ofstream outFile("output/index.html", ios::binary);
+    if (outFile.is_open()) {
+        outFile << html;
+        outFile.close();
+    } else {
+        cerr << "Failed to open output file: output/index.html" << endl;
+    }
+
+}
+
+
+//生成分类首页
+void generateCategoryIndexPage(){
+
+    //读取模板
+    string html = readTemplate("templates/category_index.html");
+    if (html.empty()) {
+        cerr << "category_index is empty. Cannot generate category index page." << endl;
+        return;
+    }
+
+    //生成HTML文件
+    ofstream outFile("output/category_index.html", ios::binary);
+    if (outFile.is_open()) {
+        outFile << html;
+        outFile.close();
+    } else {
+        cerr << "Failed to open output file: output/category_index.html" << endl;
+    }
+
+}
+
+
+//获得新闻筛选结果
+vector<News> getCategoryNews(const Category& category){
+
+    vector<News> result;
+
+    //时间分类
+    if(category.type == "time"){
+        if(category.value == "today"){
+            result = database::getTodayNews();
+        }
+        else if(category.value == "week_ago"){
+            result = database::getWeekNews();
+        }
+        else if(category.value == "month_ago"){
+            result = database::getMonthNews();
+        }
+        else if(category.value == "history"){
+            result = database::getAllNews();
+        }
+    }
+
+    //来源分类
+    else if(category.type == "source"){
+        result = database::getNewsBySource(category.value);
+    }
+
+    //主题分类
+    else if(category.type == "topic"){
+        result = database::getNewsByTopic(category.value);
+    }
+
+    return result;
+
+}
+
+
+//生成分类详细页
+void generateCategoryResultPage(const vector<News>& result, const Category& category){
+
+    //读取模板
+    string html = readTemplate("templates/category_result.html");
+    if (html.empty()) {
+        cerr << "category_result.html is empty. Cannot generate category page." << endl;
+        return;
+    }
+
+    //替换标题
+    replace(html, "{{category_title}}", category.title);
+
+    //替换新闻列表
+    replace(html, "{{news_list}}", generateAllNewsCards(result, category));
+
+    //生成HTML文件
+    ofstream outFile("output/categories/" + category.filename + ".html", ios::binary);
+    if (outFile.is_open()) {
+        outFile << html;
+        outFile.close();
+    } else {
+        cerr << "Failed to open output file: " << category.filename + ".html" << endl;
+    }
+    
+}
+
+
+//生成新闻JSON文件
+void generateNewJson(const vector<News>& newslist){
+
+    ofstream file("output/news.json", ios::binary);
+    if(!file.is_open()){
+        cout<<"Failed to create news.json"<<endl;
+        return;
+    }
+    file<<"[\n";
+    for(int i = 0; i < newslist.size(); i++){
+        const News& news = newslist[i];
+        file<<"{\n";
+        file<<"\"id\":\""<<news.id<<"\",\n";
+        file<<"\"title\":\""<<news.title<<"\",\n";
+        file<<"\"time\":\""<<news.time<<"\",\n";
+        file<<"\"content\":\""<<news.content<<"\",\n";
+        file<<"\"abstract\":\""<<news.abstract<<"\",\n";
+        file<<"\"url\":\""<<news.url<<"\",\n";
+        file<<"\"source\":\""<<news.source<<"\",\n";
+        file<<"\"image\":\""<<news.image<<"\",\n";
+        file<<"\"topic\":\""<<news.topic<<"\"\n";
+        file<<"}";
+        if(i < newslist.size() - 1){
+            file<<",";
+        }
+        file<<"\n";
+    }
+    file<<"]";
+    file.close();
+
+}
+
+
+//生成搜索页
+void generateSearchPage(){
+
+    //读取模板
+    string html = readTemplate("templates/search.html");
+    if (html.empty()) {
+        cerr << "search.html is empty. Cannot generate search page." << endl;
+        return;
+    }
+
+    //生成HTML文件
+    ofstream outFile("output/search.html", ios::binary);
+    if (outFile.is_open()) {
+        outFile << html;
+        outFile.close();
+    } else {
+        cerr << "Failed to open output file: output/search.html" << endl;
+    }
+
+}
+
+void generateNewsWebsite(){
+     database::openDb();
+
+    //生成新闻详情页
+    vector<News> newsList = database::getAllNews();
+    for(auto& news : newsList)
+    {
+        generateNewsDetailPage(news);
+    }
+    cout << "News detail pages generated successfully!" << endl;
+
+    //生成新闻JSON文件
+    generateNewJson(newsList);
+    cout << "News JSON file generated successfully!" << endl;
+
+    //生成新闻首页
+    generateNewsIndexPage(newsList);
+    cout << "News index page generated successfully!" << endl;
+
+    //生成分类首页
+    generateCategoryIndexPage();
+    cout << "Category index page generated successfully!" << endl;
+
+    //生成分类详情页
+    vector<Category> categories =
+    {
+        {
+            u8"按时间分类：今日新闻",
+            "time_today",
+            "time",
+            "today"
+        },
+        {
+            u8"按时间分类：最近一周",
+            "time_weekago",
+            "time",
+            "week_ago"
+        },
+        {
+            u8"按时间分类：最近一个月",
+            "time_monthago",
+            "time",
+            "month_ago"
+        },
+        {
+            u8"按时间分类：历史咨询",
+            "time_history",
+            "time",
+            "history"
+        },
+        {
+            u8"按来源分类：教务处",
+            "source_jwc",
+            "source",
+            "jwc"
+        },
+        {
+            u8"按来源分类：学生会",
+            "source_xsh",
+            "source",
+            "xsh"
+        },
+        {
+            u8"按来源分类：学院通知",
+            "source_xy",
+            "source",
+            "xy"
+        },
+        {
+            u8"按来源分类：学生社团",
+            "source_club",
+            "source",
+            "club"
+        },
+        {
+            u8"按主题分类：教学通知",
+            "topic_teach",
+            "topic",
+            "teach"
+        },
+        {
+            u8"按主题分类：校园活动",
+            "topic_activity",
+            "topic",
+            "activity"
+        },
+        {
+            u8"按主题分类：竞赛比赛",
+            "topic_competition",
+            "topic",
+            "competition"
+        },
+        {
+            u8"按主题分类：考试安排",
+            "topic_exam",
+            "topic",
+            "exam"
+        },
+        {
+            u8"按主题分类：科研创新",
+            "topic_research",
+            "topic",
+            "research"
+        }
+    };
+    for(auto& category : categories)
+    {
+        vector<News> result = getCategoryNews(category);
+        generateCategoryResultPage(result, category);
+    }
+    cout << "Category pages generated successfully!" << endl;
+
+    //生成搜索页
+    generateSearchPage();
+    cout << "Search page generated successfully!" << endl;
+
+    database::closeDb();
+    
+}
--- Server/js/search.js
+++ Server/js/search.js
@@ -1,93 +1,438 @@
 let input = document.getElementById("search-input");
+
 let keyword = "";
 let keywords = [];
+
+let newsList = [];
 let result = [];
+
+
+// ===============================
+// 生成新闻列表
+// ===============================
+
 function printNewsList(result){
-    let html="";
+
+    let html = "";
+
+
+    // 无搜索结果
+    if(result.length === 0){
+
+        document.getElementById("news-list").innerHTML =
+        `
+        <div class="news-card">
+            <div class="news-title">
+                暂无相关资讯
+            </div>
+        </div>
+        `;
+
+        return;
+    }
+
+
     result.forEach(news=>{
+
+
         html +=
         `
         <div class="news-card">
-        <div class="news-source"> ${highlight(news.source, keyword)} </div>
-        <div class="news-title"> ${highlight(news.title, keyword)} </div>
-        <div class="news-time"> ${highlight(news.time, keyword)} </div>
-        <div class="news-abstract"> ${highlight(news.abstract, keyword)} </div>
-        <a class="detail-btn" href="detail/${news.id}.html?from=search">查看详情</a>
+
+
+            <div class="news-source">
+                ${highlight(news.source, keywords)}
+            </div>
+
+
+            <div class="news-title">
+                ${highlight(news.title, keywords)}
+            </div>
+
+
+            <div class="news-time">
+                ${highlight(news.time, keywords)}
+            </div>
+
+
+            <div class="news-abstract">
+                ${highlight(news.abstract, keywords)}
+            </div>
+
+
+            <a class="detail-btn"
+            href="detail/${news.id}.html?from=search">
+
+                查看详情
+
+            </a>
+
+
         </div>
         `;
+
     });
+
+
     document.getElementById("news-list").innerHTML = html;
+
 }
+
+
+
+// ===============================
+// 正则特殊字符转义
+// ===============================
+
 function escapeRegExp(string){
 
-    return string.replace(/[.*+?^${}()|[\]\\]/g,'\\$&');
+    return string.replace(
+        /[.*+?^${}()|[\]\\]/g,
+        '\\$&'
+    );
 
 }
-function highlight(text, keyword){
+
+
+
+// ===============================
+// 关键词高亮
+// ===============================
+
+function highlight(text, keywords){
+
 
     if(!text){
-        return text;
+
+        return "";
+
     }
-    if(keyword==""){
+
+
+    if(keywords.length===0){
+
         return text;
+
     }
-    let result = text;
-    keywords.forEach(key => {
-        if(key.trim() !== ""){
-            let reg = new RegExp(escapeRegExp(key), "g");
-            result = result.replace(reg, `<span class='highlight'>${key}</span>`);
+
+
+
+    // 长关键词优先
+    let keys = [...keywords]
+    .sort((a,b)=>b.length-a.length);
+
+
+
+    keys.forEach(key=>{
+
+
+        if(key.trim()===""){
+
+            return;
+
         }
+
+
+
+        let reg =
+        new RegExp(
+            escapeRegExp(key),
+            "g"
+        );
+
+
+        text =
+        text.replace(
+            reg,
+            match=>
+            `<span class="highlight">${match}</span>`
+        );
+
+
     });
-    return result;
+
+
+
+    return text;
 
 }
+
+
+
+// ===============================
+// 搜索函数
+// ===============================
+
 function search(newsList){
-    let params = new URLSearchParams(window.location.search);
-    keyword = input.value.trim() || params.get("keyword") || "";
-    keywords = keyword.trim().split(/\s+/).filter(key=>key.length>0);
-    result = newsList.filter(news=>{
-        return keywords.every(key=>{
-            return(
-            (news.title && news.title.includes(key))
-            ||  
-            (news.time && news.time.includes(key))
-            ||
-            (news.abstract && news.abstract.includes(key))
-            ||
-            (news.content && news.content.includes(key))
-            ||
-            (news.source && news.source.includes(key))
-            ||
-            (news.topic && news.topic.includes(key))
-            );
+
+
+
+    let params =
+    new URLSearchParams(
+        window.location.search
+    );
+
+
+    keyword =
+    input.value.trim()
+    ||
+    params.get("keyword")
+    ||
+    "";
+
+
+
+    keywords =
+    keyword
+    .trim()
+    .split(/\s+/)
+    .filter(
+        key=>key.length>0
+    );
+
+
+
+
+    // 空关键词显示全部
+    if(keywords.length===0){
+
+        result=[...newsList];
+
+    }
+
+    else{
+
+
+        result =
+        newsList.filter(news=>{
+
+
+            return keywords.every(key=>{
+
+
+                return (
+
+                    (news.title &&
+                    news.title.includes(key))
+
+
+                    ||
+
+                    (news.time &&
+                    news.time.includes(key))
+
+
+                    ||
+
+                    (news.abstract &&
+                    news.abstract.includes(key))
+
+
+                    ||
+
+                    (news.content &&
+                    news.content.includes(key))
+
+
+                    ||
+
+                    (news.source &&
+                    news.source.includes(key))
+
+
+                    ||
+
+                    (news.topic &&
+                    news.topic.includes(key))
+
+
+                );
+
+
+            });
+
+
         });
-        
-    });
+
+
+    }
+
+
+
     printNewsList(result);
-    document.getElementById("search-keyword").innerHTML = keyword;
+
+
+
+    let keywordBox =
+    document.getElementById(
+        "search-keyword"
+    );
+
+
+    if(keywordBox){
+
+        keywordBox.innerHTML =
+        keyword;
+
+    }
+
+
 }
 
-let newsList = [];
+
+
+// ===============================
+// 初始化
+// ===============================
+
+
 fetch("json/news.json")
-.then(response => response.json())
-.then(data => {
-    newsList = data;
-    
-    let button = document.getElementById("search-button");
-    button.addEventListener("click", function(){
-        search(newsList);
-    });
+
+
+.then(response=>{
+
+
+    if(!response.ok){
+
+        throw new Error(
+            "news.json加载失败"
+        );
+
+    }
+
+
+    return response.json();
+
+
+})
+
+
+.then(data=>{
+
+
+    newsList=data;
+
+
+
+    // 页面第一次加载显示搜索结果
+
     search(newsList);
 
-    let sortDesc = document.getElementById("sort-desc");
-    sortDesc.addEventListener("click", function(){
-        result.sort(function(a,b){ return new Date(b.time)-new Date(a.time); });
-        printNewsList(result);
-    });
 
-    let sortAsc = document.getElementById("sort-asc");
-    sortAsc.addEventListener("click", function(){
-        result.sort(function(a,b){ return new Date(a.time)-new Date(b.time); });
-        printNewsList(result);
-    });
+
+
+
+    // 搜索按钮
+
+    let button =
+    document.getElementById(
+        "search-button"
+    );
+
+
+    if(button){
+
+
+        button.addEventListener(
+            "click",
+            function(){
+
+                search(newsList);
+
+            }
+        );
+
+
+    }
+
+
+
+
+
+
+    // 时间降序
+
+    let sortDesc =
+    document.getElementById(
+        "sort-desc"
+    );
+
+
+    if(sortDesc){
+
+
+        sortDesc.addEventListener(
+            "click",
+            function(){
+
+
+                result.sort(
+                    (a,b)=>
+                    new Date(b.time)
+                    -
+                    new Date(a.time)
+                );
+
+
+                printNewsList(result);
+
+
+            }
+        );
+
+    }
+
+
+
+
+
+    // 时间升序
+
+
+    let sortAsc =
+    document.getElementById(
+        "sort-asc"
+    );
+
+
+    if(sortAsc){
+
+
+        sortAsc.addEventListener(
+            "click",
+            function(){
+
+
+                result.sort(
+                    (a,b)=>
+                    new Date(a.time)
+                    -
+                    new Date(b.time)
+                );
+
+
+                printNewsList(result);
+
+
+            }
+        );
+
+
+    }
+
+
+
 })
+
+
+
+.catch(error=>{
+
+
+    console.error(
+        "新闻数据加载失败:",
+        error
+    );
+
+
+});
